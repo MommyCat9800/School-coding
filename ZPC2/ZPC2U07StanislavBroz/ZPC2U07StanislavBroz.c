f2fa3fe6eb589b1803d06070af5724bd767b3062
MommyCat <137066757+MommyCat9800@users.noreply.github.com>
2026-03-24 09:12:18 +0100

    added incCoords and drawLine

--- ZPC2/ZPC2U07StanislavBroz/ZPC2U07StanislavBroz.c
+++ ZPC2/ZPC2U07StanislavBroz/ZPC2U07StanislavBroz.c
@@ -4,6 +4,11 @@
 #define Y 30 // počet řádků souřadnice y=0..Y-1
 #define X 80 // počet sloupců souřadnice x=0..X-1
 
+typedef struct crds {
+    short x;
+    short y;
+} Crds;
+
 void printCanva(char canva[30][80]) {
     for (int y = 0; y < Y; y++) {
         for (int x = 0; x < X; x++) {
@@ -13,8 +18,54 @@ void printCanva(char canva[30][80]) {
     }
 }
 
-void addToCanva(char canva[30][80], int x, int y) {
+void incCoords(FILE* instrucFile, Crds* CurrCrds) {
+    short dx;
+    short dy;
+    fread(&dx,sizeof(short),1,instrucFile);
+    fread(&dy,sizeof(short),1,instrucFile);
+    CurrCrds->x = dx;
+    CurrCrds->y = dy;
+}
 
+void drawLine(Crds* CurrCrds,char canva[30][80] ,FILE* instrucFile) {
+    char Char;
+    short arr[3];
+
+    fread(&Char,sizeof(char),1,instrucFile);
+    fread(&arr,sizeof(short),3,instrucFile);
+
+    short dx = arr[0];
+    short dy = arr[1];
+    short len = arr[2];
+    for (int i = 0; i < len; i++) {
+        canva[CurrCrds->y][CurrCrds->x] = Char;
+        CurrCrds->x += dx;
+        CurrCrds->y += dy;
+    }
+}
+
+void addToCanva(char canva[30][80]) {
+    FILE* instrucFile = fopen("Kresba", "rb");
+    char instrucCode;
+    Crds CurrCrds;
+
+    while (!feof(instrucFile)) {
+        fread(&instrucCode,sizeof(char),1,instrucFile);
+        switch (instrucCode) {
+            case '1':
+                fread(&CurrCrds.x,sizeof(short),1,instrucFile);
+                fread(&CurrCrds.y,sizeof(short),1,instrucFile);
+                break;
+            case '2':
+                incCoords(instrucFile,&CurrCrds);
+                break;
+            case '3':
+                drawLine(&CurrCrds,canva,instrucFile);
+                break;
+            case '4':
+
+        }
+    }
 }
 
 int main() {
