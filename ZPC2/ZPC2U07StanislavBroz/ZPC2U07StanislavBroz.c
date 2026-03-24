84552cf2524c2ae724a810fc7d8b177147549a0e
MommyCat <137066757+MommyCat9800@users.noreply.github.com>
2026-03-24 08:32:29 +0100

    assignement 7

--- ZPC2/ZPC2U07StanislavBroz/ZPC2U07StanislavBroz.c
+++ ZPC2/ZPC2U07StanislavBroz/ZPC2U07StanislavBroz.c
@@ -1,16 +1,28 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#define Y 30 // počet řádků souřadnice y=0..Y-1
+#define X 80 // počet sloupců souřadnice x=0..X-1
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+void printCanva(char canva[30][80]) {
+    for (int y = 0; y < Y; y++) {
+        for (int x = 0; x < X; x++) {
+            printf("%c", canva[y][x]);
+        }
+        printf("\n");
     }
+}
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+void addToCanva(char canva[30][80], int x, int y) {
+
+}
+
+int main() {
+    char plocha[Y][X];
+    memset(plocha,' ',sizeof plocha);
+
+
+
+    printCanva(plocha);
+    return 1;
 }
