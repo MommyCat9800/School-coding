f6bc5e6413f39d2a06ba29d20db28d66e19db885
MommyCat <137066757+MommyCat9800@users.noreply.github.com>
2026-03-27 13:08:04 +0100

    not done have to finish later.... :(

--- JCP/JCP08StanislavBroz/main.cpp
+++ JCP/JCP08StanislavBroz/main.cpp
@@ -1,7 +1,44 @@
+
 #include <iostream>
+#include <string>
+
+using namespace std;
+#include "U08"
+
+void printName(int& currLineLen, int& maxLineLen, string currName) {
+    if (currLineLen + currName.length() <= maxLineLen) {
+        currLineLen += currName.length() + 1;
+        cout << currName << " ";
+    }
+    else {
+        cout << endl << currName << " ";
+        currLineLen = currName.length() + 1;
+    };
+}
 
 int main() {
+    int maxLineLen;
+    string currName;
+    bool nameEnd = false;
+    int currLineLen = 0;
+    int namesLen = Jmena.length();
+    cin >> maxLineLen;
+    for (int i = 0; i < namesLen; i++) {
+        if ( Jmena[i] >= 'A' && Jmena[i] <= 'Z') {
+            if (!nameEnd) {
+                currName.push_back( Jmena[i] );
+            } else {
+                i
+            }
 
+            nameEnd = true;
+        } else if ( Jmena[i] >= 'a' && Jmena[i] <= 'z') {
+            currName.push_back( Jmena[i] );
+        }
+        else {
+            nameEnd = true;
+        }
+    }
 
     return 1;
 }
