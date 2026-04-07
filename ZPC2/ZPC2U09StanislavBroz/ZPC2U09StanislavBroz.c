755e8d749915d831ff49b3465dc08c7d8c3ea143
MommyCat <137066757+MommyCat9800@users.noreply.github.com>
2026-04-07 09:37:52 +0200

    finished [int] month error detection

--- ZPC2/ZPC2U09StanislavBroz/ZPC2U09StanislavBroz.c
+++ ZPC2/ZPC2U09StanislavBroz/ZPC2U09StanislavBroz.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+#include "U09"
+
+#define True 1
+#define False 0
+
+int jePrestupny(const unsigned rok) {
+    return (rok % 4 == 0 && (rok % 100 != 0 || rok % 400 == 0));
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    FILE* dates = fopen("Datum.txt", "r");
+    char err[2][22] = {"chybny den nebo mesic","neni prestupny rok"};
+
+
+    char date[20];
+    while (fgets(date, sizeof(date), dates)) {
+        date[strcspn(date, "\n")] = 0;
+        if (strlen(date) == 0) continue;
+
+
+        int stringForm = False;
+        unsigned d, r, m;
+        char mmm[10];
+
+        if (sscanf(date, "%u. %u. %u", &d, &m, &r) == 3) {
+            stringForm = False;
+        }
+        else if (sscanf(date, "%u. %s %u", &d, mmm, &r) == 3) {
+           stringForm = True;
+        }
+        if (!stringForm) {
+            // ak mesiac nieje z rozsahu <1,12>
+            // ak den nieje v rozsahu <1,dny[m-1]>
+            if (m < 1 || m > 12 || d < 1 || d > dny[m-1]) {
+                printf("%d. %d. %d %s\n",d,m,r,err[0]);
+            }
+            else if ((d == 29 && m == 2) && !jePrestupny(r)) {
+                printf("%d. %d. %d %s\n",d,m,r,err[1]);
+            }
+        }
+
+    }
+
+
+
     return 0;
 }
