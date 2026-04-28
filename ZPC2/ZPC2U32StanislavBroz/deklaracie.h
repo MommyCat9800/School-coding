057f20cf7ca1e7422592f8433f95eb7ca024471e
MommyCat <137066757+MommyCat9800@users.noreply.github.com>
2026-04-28 08:21:08 +0200

    new assignement

---
+++ ZPC2/ZPC2U32StanislavBroz/deklaracie.h
@@ -0,0 +1,13 @@
+#ifndef POLE_H
+#define POLE_H
+
+#define false 0
+#define true 1
+typedef char bool;
+
+// Prototypy funkcí
+int Najit(int cislo);
+bool Pridat(int cislo);
+bool Odebrat(int cislo);
+
+#endif
---
+++ ZPC2/ZPC2U32StanislavBroz/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define false 0
+#define true 1
+typedef char bool;
+
+
+int Najit(int elem);
+bool Pridat(int elem);
+bool Odebrat(int elem);
+
+int main(void) {
+
+    return 0;
+}
